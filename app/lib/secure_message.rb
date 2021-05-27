6507a3972538f3a5431cc299af6c16facd3f354e
nathansetyawan96 <nathan.setyawan96@gmail.com>
2021-05-28 01:28:25 +0800

    bug: uninitialized constant Rewards::App::SecureSession

---
+++ app/lib/secure_message.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'rbnacl'
+
+class SecureMessage
+  def self.encoded_random_bytes(length)
+    bytes = RbNaCl::Random.random_bytes(length)
+    Base64.strict_encode64 bytes
+  end
+
+  def self.generate_key
+  endoded_random_bytes(RbNaCl::SecretBox.key_bytes)
+  end
+
+  def self.setup(msg_key)
+    @msg_key = msg_key
+  end
+
+  def self.key
+    @key ||= Base64.strict_decode64(@msg_key)
+  end
+
+  def self.encrypt(message)
+    return nill unless message
+
+    message_json = message.to_json
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    ciphertext = simple_box.encrypt(message_json)
+    Base64.urlsafe_encode64(ciphertext)
+  end
+
+  def self.decrypt(ciphertext64)
+    return nil unless ciphertext64
+
+    ciphertext = Base64.urlsafe_decode64(ciphertext64)
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    message_json = simple_box.decrypt(ciphertext)
+    JSON.parse(message_json)
+  end
+end
---
+++ app/lib/secure_session.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'redis'
+require_relative 'secure_message'
+
+class SecureSession
+  def self.setup(redis_url)
+    @redis_url = redis_url
+  end
+
+  def self.generate_secret
+    SecureMessage.encoded_random_bytes(64)
+  end
+
+  def self.wipe_redis_sessions
+    redis = Redis.new(url: @redis_url)
+    redis.key.each { |session_id| redis.del session_id }
+  end
+
+  def initialize(session)
+    @session = session
+  end
+
+  def set(key, value)
+    @session[key] = SecureMessage.encrypt(value)
+  end
+
+  def get(key)
+    return nil unless @session[key]
+    SecureMessage.decrypt(@session[key])
+  end
+
+  def delete(key)
+    @session.delete(key)
+  end
+end
