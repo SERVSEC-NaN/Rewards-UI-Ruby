ae6e35a247c63c692a35cc7e6d1f881a488a906a
nathansetyawan96 <nathan.setyawan96@gmail.com>
2021-06-17 12:09:03 +0800

    feat: authetication token works

--- app/services/authenticate_account.rb
+++ app/services/authenticate_account.rb
@@ -7,19 +7,24 @@ module Rewards
   class AuthenticateAccount
     class UnauthorizedError < StandardError; end
 
+    class ApiServerError < StandardError; end
+
     def initialize(config)
       @config = config
     end
 
     def call(email:, password:)
-      response = HTTP.post(
-        "#{@config.API_URL}/auth/authenticate",
-        json: { email: email, password: password }
-      )
+      response = HTTP.post("#{@config.API_URL}/auth/authenticate", json: { email: email, password: password })
 
       raise(UnauthorizedError) unless response.code == 200
+      raise(ApiServerError) if response.code != 200
 
-      response.parse['attributes']
+      account = JSON.parse(response.to_s)['attributes']
+
+      {
+        account: account['account']['attributes'],
+        auth_token: account['auth_token']
+      }
     end
   end
 end
